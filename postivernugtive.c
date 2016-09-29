86134369a9865aff697a92d8578f1ad908f5abc0
paiyakarthik <karthik27061995@gmail.com>
2016-09-29 16:43:40 +0530

    Add files via upload

---
+++ postivernugtive.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n==0)
+{printf("the numbr is zero");
+}
+if(n>0)
+printf("the number is postive");
+else
+printf("the numbe is negtive");
+getch();
+}
