a4b78ee32dcd1f3b91a1a6ea7f0e5e58b980dfb7
paiyakarthik <karthik27061995@gmail.com>
2016-09-29 17:03:38 +0530

    Add files via upload

---
+++ charrnum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+int main()
+{
+char m;
+scanf("%c",&m);
+if(isdigit(check))
+{
+printf(" u enter number");
+}
+  else
+	{	
+  	printf(" u enter character");
+	}
+return 0;
+}
