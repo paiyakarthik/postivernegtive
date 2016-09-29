8989b2810cdec7f636f5c8161ce2b28e5225e390
paiyakarthik <karthik27061995@gmail.com>
2016-09-29 16:54:37 +0530

    Add files via upload

---
+++ vowelrcons.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char in;
+scanf("%c",&in);
+switch(in)
+{
+case 'a':
+printf("\nvowel");
+break;
+case 'e':
+printf("\nvowel");
+break;
+case 'i':
+printf("\nvowel");
+break;
+case 'o':
+printf("\nvowel");
+break;
+case 'u':
+printf("\nvowel");
+break;
+case 'A':
+printf("\nvowel");
+break;
+case 'E':
+printf("\nvowel");
+break;
+case 'I':
+printf("\nvowel");
+break;
+case 'O':
+printf("\nvowel");
+break;
+case 'U':
+printf("\nvowel");
+break;
+default:
+printf("\nConsonant");
+}
+return 0;
+}
